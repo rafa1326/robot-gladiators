f8e007ba1efcc37f9fab48a4a684edfec6ead22e
rafa1326 <phillyaryeh@gmail.com>
2022-04-01 12:00:36 -0400

    console log function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,7 @@
-window.alert("This is an alert! JavaScript is running!");
+
+
+ var playerName = window.prompt("What is your robot's name?");
+ console.log(playerName);
+ console.log("This logs a string, good for leaving yourself a message ");
+ console.log(10+10);
+ console.log("Our robots name is " + playerName);
