c41abd3c1f33e3a118fbf2dd432ea61532acde1a
rafa1326 <phillyaryeh@gmail.com>
2022-04-05 13:52:09 -0400

    debugging code

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,7 +10,8 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 var fight = function(enemyName ){
-  window.alert("Welcome to Robot Gladiators!");
+  while(enemyHealth > 0) {
+
   var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
   // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -45,13 +46,15 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 } else {
   window.alert("You need to choose a valid option. Try again!");
 }
+  }
 }
 
-
-for(var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames [i])
+for (var i = 0; i < enemyNames.length; i++) {
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  fight(pickedEnemyName);
 }
-
+  
 
 
 
