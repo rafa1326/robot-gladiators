70a365650b844b4de4717417d0395a8a9c6f4300
rafa1326 <phillyaryeh@gmail.com>
2022-04-20 15:53:23 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -23,14 +23,21 @@ var fight = function(enemyName ){
     if (confirmSkip) {
       window.alert(playerName + ' has decided to skip this fight. Goodbye!');
       // subtract money from playerMoney for skipping
-      playerMoney = playerMoney - 10;
+      playerMoney = Math.max(0, playerMoney - 10);
       console.log("playerMoney", playerMoney)
       break;
     }
   }
 
   // remove enemy's health by subtracting the amount set in the playerAttack variable
-  enemyHealth = enemyHealth - playerAttack;
+  var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+  
+    return value;
+  };
+  var damage = randomNumber(playerAttack - 3, playerAttack);
+
+enemyHealth = Math.max(0, enemyHealth - damage);
   console.log(
     playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
   );
@@ -48,7 +55,9 @@ var fight = function(enemyName ){
   }
 
   // remove players's health by subtracting the amount set in the enemyAttack variable
-  playerHealth = playerHealth - enemyAttack;
+  var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+playerHealth = Math.max(0, playerHealth - damage);
   console.log(
     enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
   );
@@ -72,7 +81,8 @@ var startGame = function() {
   
   for (var i = 1; i < enemyNames.length; i++) {
   var pickedEnemyName = enemyNames[i];
-  enemyHealth = 50;
+  
+  enemyHealth = randomNumber(40,60);
   
   fight(pickedEnemyName);
  
@@ -178,16 +188,15 @@ switch (shopOptionPrompt) {
 }
 
 
-
-
-
-
 };
 startGame();
 
 
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
 
-
+  return value;
+};
 
  
   
